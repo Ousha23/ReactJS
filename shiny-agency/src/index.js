41747a4ea1e3b552d850c3a574bf7efd0ee63c04
bouchra <bouchra.laaz@gmail.com>
2024-04-20 09:47:15 +0200

    Partagez vos données avec le Contexte et useContext - Incorporez des données dans une application React avec les hooks - Créez une application React complète - Tuto shiny-agence OpenClassRoom

--- shiny-agency/src/index.js
+++ shiny-agency/src/index.js
@@ -7,17 +7,17 @@ import Header from './components/Header'
 import Error from './components/Error'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
-import { createGlobalStyle } from 'styled-components'
+import GlobalStyle from './utils/style/GlobalStyle'
+import { ThemeProvider, SurveyProvider } from './utils/context'
+import Footer from './components/Footer'
 
-const GlobalStyle = createGlobalStyle`
-    * {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-`
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+    <ThemeProvider>
+    <SurveyProvider>
       <GlobalStyle />
       <Header />
       <Routes>
@@ -27,6 +27,9 @@ ReactDOM.render(
         <Route path="/results" element={<Results />} />
         <Route path="/freelances" element={<Freelances />} />
       </Routes>
+      <Footer />
+      </SurveyProvider>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
